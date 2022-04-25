14f89a87e506e5ace3dc7ad3e40aad058852740b
SaiLaks8 <83442017+SaiLaks8@users.noreply.github.com>
2022-04-25 13:18:45 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBNqjizthRffV2IQKUHTiQyq7fev4g3i9Q",
+    authDomain: "project-94-cont.firebaseapp.com",
+    projectId: "project-94-cont",
+    storageBucket: "project-94-cont.appspot.com",
+    messagingSenderId: "381827167303",
+    appId: "1:381827167303:web:17d2cb41e257fa6aeb2d62"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
